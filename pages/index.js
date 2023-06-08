7f118f3918887285bfe2365c6a8eb3e77d33452a
swalekhan <swalekhan1522@gmail.com>
2023-06-08 18:38:24 +0530

    vercel

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,9 @@ export default function Home() {
       <Head>
         <title>Sabloo Marble</title>
         <meta name="description" content="Welcome to Sabloo Marble! Explore our extensive collection of high-quality tiles, lighting solutions, wooden materials, structural materials, and paints. Find the perfect tiles for your floors, walls, and backsplashes. Discover stylish lighting options to illuminate your space. Browse our selection of wooden materials for construction and woodworking projects. We also offer a range of structural materials for your building needs. Enhance your spaces with our premium paints in various colors and finishes. Shop now and bring your vision to life with Sabloo Marble." />
-        </Head>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
 
       <HeroBanner items={homeBannerAssets} />
       <Category items={homeCategory} title={"Shop by Category"} />
